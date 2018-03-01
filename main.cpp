696651c9ec671ee1a1709505fc6d25bcbece03b1
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-03-01 11:21:13 -0800

    added lots of stuff

--- main.cpp
+++ main.cpp
@@ -5,7 +5,43 @@ using namespace std;
 
 int main()
 {
+  int total = 0;
   SecretDoor game;
-    
+  int numOfPlays;
+  int wins =0;
+  int loss =0;
+  cout << "How many times do you want to play?" << endl;
+  cin >> numOfPlays;
+
+
+  do
+  {   
+      total++;
+      game.guessDoorC();
+      game.guessDoorC();
+      if (game.isWinner())
+      { 
+        
+        
+        wins++;
+      }else
+      {
+       loss++;
+      }
+
+
+
+  game.newGame();
+   
+  }while ( total < numOfPlays);
+
+cout << "Your total games is: " << total;
+cout << "you won: " << wins  << " times" << endl;
+cout << "you loss: " << loss << " times" << endl;
+
+
+
+
+
   return 0;
 }
